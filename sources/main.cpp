9081a5f8301c524c2bc6cb29ba0d86680fe00521
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-12-09 18:09:54 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,8 +2,5 @@
 #include <multithreads.hpp>
 int main() {
 	Multithreads m;
-	logging::add_file_log("home/travis/build/bmstu-iu8-34-cpp-2018/lab-06-multithreads-Avsyankaa/sample.log");
-  logging::add_common_attributes();
-  src::severity_logger< severity_level > lg;
-    m.Make_counting_in_threads(4, lg);
+    m.Make_counting_in_threads(4);
 }
