68e5e3ad5e2bacb616ddbced6168b6154c533915
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-12-09 17:56:04 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,12 @@
 // Copyright 2018 Avsyankaa <Avsyankaa@gmail.com>
 #include <multithreads.hpp>
+void init() {
+  logging::add_file_log("home/travis/build/bmstu-iu8-34-cpp-2018/lab-06-multithreads-Avsyankaa/sample.log");
+  logging::core::get()->set_filter
+    (
+        logging::trivial::severity >= logging::trivial::info
+    );
+} 
 
 int main() {
 	Multithreads m;
