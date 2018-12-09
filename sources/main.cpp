9f0417fed674d22082585d75a98c9359514b5e52
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-12-09 18:05:00 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,16 +1,8 @@
 // Copyright 2018 Avsyankaa <Avsyankaa@gmail.com>
 #include <multithreads.hpp>
-void init() {
-  logging::add_file_log("home/travis/build/bmstu-iu8-34-cpp-2018/lab-06-multithreads-Avsyankaa/sample.log");
-  logging::core::get()->set_filter
-    (
-        logging::trivial::severity >= logging::trivial::info
-    );
-} 
-
 int main() {
 	Multithreads m;
-	init();
+	logging::add_file_log("home/travis/build/bmstu-iu8-34-cpp-2018/lab-06-multithreads-Avsyankaa/sample.log");
   logging::add_common_attributes();
   src::severity_logger< severity_level > lg;
     m.Make_counting_in_threads(4, lg);
