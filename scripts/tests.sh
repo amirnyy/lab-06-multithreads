58edd007f38d01a75a5818ad55385f7c4f1e112f
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-12-09 14:09:19 +0300

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,6 +6,8 @@ export CTEST_OUTPUT_ON_FAILURE=true
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target lcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
 ls
 cd _builds
+ls
+cd Debug
 ./tests
 gcovr -r  .
 
